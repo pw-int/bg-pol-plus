3aebee62066216e56c5c8f0e5426781029895e77
Ivan Dubynets <ivan@dubisoft.solutions>
2024-01-20 21:15:50 +0100

    removed unused stuff from js

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,7 @@
-//= https://www.google.com/recaptcha/api.js
-
-
-$(function() {
-    initSelectPicker();
+(function () {
     initFormValidation();
-    initRecaptcha('6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI', ".grecaptcha");
-});
+})();
 
-function initSelectPicker() {
-    $('.selectpicker').selectpicker();
-    const selectPickers = document.querySelectorAll('.selectpicker')
-    selectPickers.forEach(function(picker) {
-        picker.addEventListener('invalid', (e) => {
-            console.log("e", picker.value);
-            const parentControl = picker.closest('.dropdown.bootstrap-select');
-            if (parentControl) {
-                parentControl.classList.add('is-invalid')
-                picker.addEventListener("change", (e) => {
-                    parentControl.classList.remove('is-invalid')
-                }, { once: true });
-            }
-        })
-    });
-}
 
 /**
  * Validation
@@ -62,21 +41,4 @@ function genericFormsValidation(event, form) {
     }
 
     form.classList.add('was-validated')
-}
-
-/**
- * Recaptcha
- */
-function initRecaptcha(sitekey, containerSelector) {
-    const recaptchaContainers = document.querySelectorAll(containerSelector);
-    recaptchaContainers.forEach(recaptchaContainer => {
-        // If reCAPTCHA is still loading, grecaptcha will be undefined.
-        recaptchaContainer.setAttribute("data-theme", "dark");
-        grecaptcha.ready(function() {
-            grecaptcha.render(recaptchaContainer, {
-                sitekey: sitekey,
-            });
-
-        });
-    })
 }
